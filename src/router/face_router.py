13878466de4bab7746d242584d5cb30adf4f0f81
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-08-29 09:37:01 +0700

    add handle processing for base64 image

--- src/router/face_router.py
+++ src/router/face_router.py
@@ -1,3 +1,7 @@
+import base64
+from datetime import date
+import os
+import uuid
 from fastapi import APIRouter, Form, HTTPException, UploadFile
 from src.service.face_service.store_face_service import StoreFaceParam, store_face
 from src.service.face_service.verify_face_service import verify_face
@@ -18,11 +22,34 @@ async def store(user_id: int = Form(), images: list[UploadFile] = []):
 
 
 @router.post("/verify")
-async def verify(user_id: int = Form(), image: UploadFile | None = None):
-    if image is None:
+async def verify(
+    user_id: int = Form(),
+    image: UploadFile | None = None,
+    image_base64: str | None = None,
+):
+    img_path = ""
+
+    if image is None and image_base64 is None:
         raise HTTPException(status_code=422, detail="Image not found")
 
-    result = await verify_face(user_id, image.file)
+    filename = "assets/" + uuid.uuid4().hex + ".jpg"
+
+    if image_base64 is not None:
+        with open("image.jpg", "wb") as file:
+            base64_image = str(image_base64).split(";base64,")[1]
+            decoded_image = base64.b64decode(base64_image)
+
+            file.write(decoded_image)
+            img_path = filename
+
+    if image is not None:
+        with open(filename, "wb") as file:
+            file.write(image.file.read())
+            img_path = filename
+
+    result = await verify_face(user_id, img_path)
+
+    os.remove(img_path)
 
     if not result.verified:
         raise HTTPException(401, "user face not recognized")
--- src/service/face_service/verify_face_service.py
+++ src/service/face_service/verify_face_service.py
@@ -1,7 +1,7 @@
 import os
 
 from deepface import DeepFace
-from fastapi import HTTPException
+from fastapi import HTTPException, UploadFile
 import numpy
 from pydantic import BaseModel
 
@@ -15,24 +15,15 @@ class VerifyFaceResult(BaseModel):
     confidence: float
 
 
-async def verify_face(user_id: int, image: str) -> VerifyFaceResult:
+async def verify_face(user_id: int, img_path: str) -> VerifyFaceResult:
     user_folder = user_face_dir(user_id)
-    print(user_folder)
 
     if not os.path.exists(user_folder):
         raise HTTPException(404, detail="Face recognition not found")
-    #
-    # for file in os.scandir(user_folder):
-    #     encoded_vector = numpy.load(file.path)
-    #     encoded_images.append(encoded_vector)
 
     results = DeepFace.find(
-        img_path=image, db_path="assets/faces/1", model_name=model_name
+        img_path=img_path, db_path=user_folder, model_name=model_name
     )
-    # unknown = face_recognition.load_image_file(image)
-    # unknown_encoding = face_recognition.face_encodings(unknown)[0]
-    #
-    # distances = face_recognition.face_distance(encoded_images, unknown_encoding)
 
     if len(results) == 0:
         raise HTTPException(401, detail="Face recognition not found")
