fb9297614667ca0379cacf0b08fd0bbf2a9fc0eb
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-09-08 15:50:43 +0700

    add environment check

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 import uvicorn
 from dotenv import load_dotenv
 
+from src.utils import env
+
 load_dotenv()
 
 model_name = os.getenv("MODEL_NAME") or "Facenet"
 
 if __name__ == "__main__":
-    uvicorn.run("src.app:app", host="0.0.0.0", port=4000, reload=True)
+    uvicorn.run("src.app:app", host="0.0.0.0", port=4000, reload=env.is_development())
--- src/utils/env.py
+++ src/utils/env.py
@@ -1,7 +1,13 @@
 import os
 
+app_env = os.getenv("APP_ENV") or "development"
+
 api_lms_url = os.getenv("API_LMS_URL") or "http://localhost:8000/api/development"
 api_lms_key = os.getenv("API_LMS_KEY") or "1234"
 
 model_name = os.getenv("MODEL_NAME") or "Facenet"
 backend_detector = os.getenv("BACKEND_DETECTOR") or "fastmtcnn"
+
+
+def is_development():
+    return app_env == "development"
